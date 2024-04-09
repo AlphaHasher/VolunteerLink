098b7d3af93f1b69e283d7be21ac818e83f37a00
Dan <agdaniel7@gmail.com>
2024-04-09 14:28:20 -0700

    properly staged api keys and mongo url

--- VolunteerLink/src/main/java/com/VolunteerLink/app/App.java
+++ VolunteerLink/src/main/java/com/VolunteerLink/app/App.java
@@ -1,13 +1,34 @@
 package com.VolunteerLink.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+// Java Imports
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+// MongoDB Imports
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        // Connect to MongoDB
+        String uri = "";
+
+        try (InputStream input = App.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            prop.load(input);
+            uri = prop.getProperty("MongoDBKey");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
