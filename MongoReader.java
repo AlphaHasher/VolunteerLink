638c1dca25187c449645f60c0fde878746593c4b
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2024-04-09 01:42:18 -0700

    MongoDB Connection Setup

---
+++ MongoReader.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.io.*;
+
+//MongoDB Dependency imports
+
+class MongoReader {
+	public static void main(String args[])
+	{
+		
+		
+	}
+}
