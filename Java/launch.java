2179a636c1fc03d0b243fd586f67b94406fd766f
Dan <agdaniel7@gmail.com>
2024-04-09 11:32:21 -0700

    added config file and some clean up

--- Java/launch.java
+++ Java/launch.java
@@ -1,7 +1,7 @@
-class launch {
+import java.io.*;
+public class launch {
+    public static void main(String args[]) {
 
-	public static void main(String args[])
-	{
-		System.out.println("Hello, World");
-	}
+        System.out.print("Hello World");
+    }
 }
