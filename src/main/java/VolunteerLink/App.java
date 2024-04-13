df88f161f85f070d501a08d02f2e304fdde4da00
Dan <agdaniel7@gmail.com>
2024-04-12 23:07:15 -0700

    database singleton design pattern

--- src/main/java/VolunteerLink/App.java
+++ src/main/java/VolunteerLink/App.java
@@ -28,54 +28,20 @@ public class App
         // Optimization checker
         long startTime = System.nanoTime();
 
-
-        // Store the MongoDB URI
-        String uri = "";
-
-        try (InputStream input = App.class.getClassLoader().getResourceAsStream("config.properties")) {
-            Properties prop = new Properties();
-            if (input == null) {
-                System.out.println("Sorry, unable to find config.properties\nVerify that the config.properties file is in the VolunteerLink/scr/main/java folder.");
-                return;
-            }
-            prop.load(input);
-            uri = prop.getProperty("MongoDBKey");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        // Connecting to the MongoDB database
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("VolunteerLink");
-
-            Admin admin = new Admin(mongoClient, database);
-            EventManager eventManager = new EventManager(mongoClient, database);
-
-            MongoCollection<Document> Events = database.getCollection("Events");
-
-
-            System.out.println("Connected to the database.");
-
-            // Event event = new Event(mongoClient, database);
-            // String[] pendingEvents = event.getPendingEvents();
-            // for (int i = 0; i < pendingEvents.length; i++) {
-            //     System.out.println(pendingEvents[i]);
-            // }
-
             // Testing section for Colin
-            if (true) {
-                //Event testEvent = new Event("eventName", "description", "startDate", "endDate", "location", 2, 1, "Pending"); // test import
-                //Document testDocument = testEvent.toDocument();
-                //Events.insertOne(testDocument);
-                Event testEvent = new Event(mongoClient, database);
-                //System.out.println(testUser.getEvents());
-                //testUser.viewEventNames();
-                String[] arr = testEvent.getLocations();
-                for (int i = 0; i < arr.length - 1; ++i) {
-                    System.out.println("EventLocations: " + i + " " + arr[i]);
-                }
-                //testUser.viewEventNames();
+            //Event testEvent = new Event("eventName", "description", "startDate", "endDate", "location", 2, 1, "Pending"); // test import
+            //Document testDocument = testEvent.toDocument();
+            //Events.insertOne(testDocument);
+            
+            Event testEvent = new Event(mongoClient, database);
+            //System.out.println(testUser.getEvents());
+            //testUser.viewEventNames();
+            String[] arr = testEvent.getLocations();
+            for (int i = 0; i < arr.length - 1; ++i) {
+                System.out.println("EventLocations: " + i + " " + arr[i]);
             }
+            //testUser.viewEventNames();
+
             // Colin testing section end
 
             // ----------------- User get/set Priority Test -----------------
@@ -108,6 +74,5 @@ public class App
             // admin.denyEvent("Sample Event");
             // admin.approveEvent("Sample Event");
             // ----------------- Event Approval Test -----------------
-        }
     }
 }
---
+++ src/main/java/VolunteerLink/Database.java
@@ -0,0 +1,48 @@
+package VolunteerLink;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class Database {
+
+    private static Database instance;
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+
+    private static final String DATABASE_NAME = "VolunteerLink";
+
+    private Database() {
+        String uri;
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties\nVerify that the config.properties file is in the VolunteerLink/src/main/java folder.");
+                return;
+            }
+            prop.load(input);
+            uri = prop.getProperty("MongoDBKey");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        mongoClient = MongoClients.create(uri);
+        database = mongoClient.getDatabase(DATABASE_NAME);
+    }
+
+    // test methods
+    public static synchronized Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public MongoDatabase getDatabase() {
+        return database;
+    }
+}
